f09846367b675e58d1e26444cef1ffc031c8aba1
darkregliss <pomart.thomas1@gmail.com>
2021-03-29 15:14:29 +0200

    correctif categoryz

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-// const userRouter = require('./users.route');
+const userRouter = require('./users.route');
+const productRouter = require('./products.route');
+const categoryRouter = require('./categoryz.route');
 
 
-// router.use(userRouter);
+router.use(userRouter);
+router.use(productRouter);
+router.use(categoryRouter);
 
 module.exports = router;
