38381bbb783a140ae50043ac35688269b674f55f
darkregliss <pomart.thomas1@gmail.com>
2021-04-06 17:05:01 +0200

    category

--- src/controllers/categoryz.controller.js
+++ src/controllers/categoryz.controller.js
@@ -41,3 +41,33 @@ exports.getCategoryz = (req, res) => {
     })
     .catch((err) => res.send(err));
 };
+
+exports.update = (req, res) => {
+    Category.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            title: req.body.title,
+        }
+    )
+    .then((data) => {
+        res.json({
+            message :" suce ta mammouth",
+            data: data
+        });
+    }).catch((err) => {
+        console.log(err.message);
+    })
+};
+
+exports.removeOne = (req, res) => {
+	Category.findByIdAndRemove(req.params.id)
+	.then((data) => {
+		// if (!data) {
+		// 	res.status(404).send({
+		// 		message: `User with id ${req.params.id} not found`
+		// 	})
+		// }
+		res.send(data);
+	})
+	.catch((err) =>	res.send(err));
+};
--- src/routes/categoryz.route.js
+++ src/routes/categoryz.route.js
@@ -5,5 +5,7 @@ const category = require('../controllers/categoryz.controller');
 router.post('/categoryz', category.create);
 router.get('/categoryz/:id', category.getCategory);
 router.get('/categoryz/', category.getCategoryz);
+router.post('/categoryz/edit/:id', category.update);
+router.get('/categoryz/delete/:id', category.removeOne);
 
 module.exports = router;
